e0b334f50b7e9b9330d424e1914015f653f8b00b
sousou1216 <sousou1216@yahoo.co.jp>
2024-12-04 23:04:56 +0900

    パス取得の修正

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-    # NOTE: Windowsではパス区切りが \ のため問題となる可能性がありjoinを使う
-    path = f"{os.path.dirname(__file__)}/src/path.py"
+    # NOTE: Windowsではパス区切りが \ となり問題となる可能性があるためjoinを使う
+    # path = f"{os.path.dirname(__file__)}/src/path.py"
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "src", "path.py")
     subprocess.run(["python", path])
